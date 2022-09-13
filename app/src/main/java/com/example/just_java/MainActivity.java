149cc89db2c90a6fa8b9bd5f8ba8cdb6c16680d4
Lizzie <lizziemeis924@gmail.com>
2022-09-13 11:39:55 -0500

    completely finished coffee app

--- app/src/main/java/com/example/just_java/MainActivity.java
+++ app/src/main/java/com/example/just_java/MainActivity.java
@@ -23,8 +23,14 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(quantity);
-        displayPrice(quantity*5);
+        String priceMessage = "Total: $" + (quantity * 5) + "\nThank you!";
+        displayMessage(priceMessage);
+    }
+
+    //displays the given text on the screen
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 
     /**
