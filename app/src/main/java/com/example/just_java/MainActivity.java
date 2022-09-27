df0f909945eb172b6d35b8f3ed096682379b3f39
Lizzie <lizziemeis924@gmail.com>
2022-09-27 15:42:49 -0500

    completely finished just java app

--- app/src/main/java/com/example/just_java/MainActivity.java
+++ app/src/main/java/com/example/just_java/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.just_java;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import java.text.NumberFormat;
 
 /**
@@ -23,20 +27,61 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        String priceMessage = "Total: $" + (quantity * 5) + "\nThank you!";
+        //get their name into string form
+        EditText nameField = (EditText) findViewById(R.id.name);
+        String name = nameField.getText().toString();
+
+        //see if they want whipped cream and/or chocolate
+        CheckBox whip = (CheckBox) findViewById(R.id.whipped_cream);
+        boolean ifWhip = whip.isChecked();
+        CheckBox chocolate = (CheckBox) findViewById(R.id.chocolate);
+        boolean ifChoc = chocolate.isChecked();
+
+        //calculate the price
+        int pricePerCoffee = 5;
+        if (ifWhip) {
+            pricePerCoffee += 1; //whipped cream costs $1
+        }
+        if (ifChoc) {
+            pricePerCoffee += 2; //chocolate costs $2
+        }
+        int price = pricePerCoffee * quantity;
+
+        String priceMessage = createOrderSummary(name, price, ifWhip, ifChoc);
+
         displayMessage(priceMessage);
+    }
+
+    private String createOrderSummary(String name, int price, boolean addWhip, boolean addChoc) {
+        String message = "Name: " + name;
+        message += "\nQuantity: " + quantity;
+        if (addWhip) {
+            message += "\nAdd whipped cream";
+        }
+        else {
+            message += "\nNo whipped cream";
+        }
+        if (addChoc) {
+            message += "\nAdd chocolate";
+        }
+        else {
+            message += "\nNo chocolate";
+        }
+        message += "\nTotal: $" + price;
+        message += "\nThank you!";
+        return message;
     }
 
     //displays the given text on the screen
     private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
@@ -45,17 +90,26 @@ public class MainActivity extends AppCompatActivity {
      * This method displays the given price on the screen.
      */
     private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        TextView priceTextView = (TextView) findViewById(R.id.order_summary_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 
     public void increment(View view) {
+        if (quantity >= 100) {
+            //show error message using Toast
+            Toast.makeText(this, "You cannot have more than 100 coffees", Toast.LENGTH_SHORT).show();
+            return; //exit method
+        }
         quantity += 1;
-        display(quantity);
+        displayQuantity(quantity);
     }
 
     public void decrement(View view) {
+        if (quantity <= 1) {
+            Toast.makeText(this, "You cannot have less than 1 coffee", Toast.LENGTH_SHORT).show();
+            return; //exit method w/out decrementing
+        }
         quantity -= 1;
-        display(quantity);
+        displayQuantity(quantity);
     }
 }
